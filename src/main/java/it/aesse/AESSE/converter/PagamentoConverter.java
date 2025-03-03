e19fe6d2c69caea11950802e29ccc008a98bb58a
SansRS5758 <dalentinog@gmail.com>
2025-03-03 14:41:15 +0100

    Refactor PagamentoConverter: improve formatting and ensure consistent code style

--- src/main/java/it/aesse/AESSE/converter/PagamentoConverter.java
+++ src/main/java/it/aesse/AESSE/converter/PagamentoConverter.java
@@ -1,20 +1,22 @@
+package it.aesse.AESSE.converter;
+
 import it.aesse.AESSE.converter.AbstractConverter;
-    import it.aesse.AESSE.dto.PagamentoDto;
-    import it.aesse.AESSE.model.Pagamento;
-    import org.modelmapper.ModelMapper;
-    import org.springframework.stereotype.Component;
+import it.aesse.AESSE.dto.PagamentoDto;
+import it.aesse.AESSE.model.Pagamento;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
-    @Component
-    public class PagamentoConverter extends AbstractConverter<Pagamento, PagamentoDto> {
-        private ModelMapper modelMapper = new ModelMapper();
+@Component
+public class PagamentoConverter extends AbstractConverter<Pagamento, PagamentoDto> {
+    private ModelMapper modelMapper = new ModelMapper();
 
-        @Override
-        public Pagamento toEntity(PagamentoDto pagamentoDto) {
-            return modelMapper.map(pagamentoDto, Pagamento.class);
-        }
-
-        @Override
-        public PagamentoDto toDTO(Pagamento pagamento) {
-            return modelMapper.map(pagamento, PagamentoDto.class);
-        }
+    @Override
+    public Pagamento toEntity(PagamentoDto pagamentoDto) {
+        return modelMapper.map(pagamentoDto, Pagamento.class);
     }
+
+    @Override
+    public PagamentoDto toDTO(Pagamento pagamento) {
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+}
