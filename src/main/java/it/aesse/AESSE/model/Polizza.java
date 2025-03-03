7d97a5e018f763e77fb18355d2fe4cf64b607160
eaann <eaannist@gmail.com>
2025-03-03 15:24:09 +0100

    Modifica Types -> Polizza

--- src/main/java/it/aesse/AESSE/model/Polizza.java
+++ src/main/java/it/aesse/AESSE/model/Polizza.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -14,25 +16,25 @@ public class Polizza {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "id_polizza")
-        private Integer id_polizza;
+        private Long id_polizza;
 
         @Column(name = "data_inizio")
-        private String data_inizio;
+        private LocalDate data_inizio;
 
         @Column(name = "data_scadenza")
-        private String data_scadenza;
+        private LocalDate data_scadenza;
 
         @Column(name = "tipo")
         private String tipo;
 
         @Column(name = "importo_rata")
-        private int importo_rata;
+        private BigDecimal importo_rata;
 
         @Column(name = "importo")
-        private int importo;
+        private BigDecimal importo;
 
         @Column(name = "premio")
-        private int premio;
+        private BigDecimal premio;
 
         @Column(name = "stato")
         private String stato;
