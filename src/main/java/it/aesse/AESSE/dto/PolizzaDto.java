d954840345405b596440a4b6e06dc7c05e5cb75a
eaann <eaannist@gmail.com>
2025-03-03 15:29:12 +0100

    Modifica Types -> PolizzaDTO

--- src/main/java/it/aesse/AESSE/dto/PolizzaDto.java
+++ src/main/java/it/aesse/AESSE/dto/PolizzaDto.java
@@ -2,18 +2,20 @@ package it.aesse.AESSE.dto;
 
 import lombok.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
 public class PolizzaDto {
-    private Integer id_polizza;
-    private Integer costo;
-    private String data_inizio;
-    private String data_scadenza;
+    private Long id_polizza;
+    private LocalDate data_inizio;
+    private LocalDate data_scadenza;
     private String tipo;
-    private int importo_rata;
-    private int premio;
+    private BigDecimal importo_rata;
+    private BigDecimal importo;
+    private BigDecimal premio;
     private String stato;
-    private List<ClienteDto> cliente;
-    private List<BeneDto> bene;
+    private Long id_cliente;
+    private Long id_bene;
 }
