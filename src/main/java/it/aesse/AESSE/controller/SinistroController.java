5f621d305ece2bf34f07617428ecd94ccdd42b72
eaann <eaannist@gmail.com>
2025-03-03 16:00:00 +0100

    Modifica Controllers

--- src/main/java/it/aesse/AESSE/controller/SinistroController.java
+++ src/main/java/it/aesse/AESSE/controller/SinistroController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Sinistro")
-@CrossOrigin(origins = "http//localhost:8080")
+@CrossOrigin(origins = "http://localhost:8080")
 public class SinistroController extends AbstractController<SinistroDto> {
     @Autowired
     private SinistroService sinistroService;
