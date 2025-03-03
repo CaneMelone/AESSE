f3aded63ff2ea841f20b75b7b113c4b96924c30d
Vincenzo Capasso <77034766+CaneMelone@users.noreply.github.com>
2025-03-03 11:28:19 +0100

    Add files via upload

---
+++ src/main/java/it/aesse/AESSE/AesseApplication.java
@@ -0,0 +1,13 @@
+package it.aesse.AESSE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AesseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/aesse/AESSE/AesseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aesse.AESSE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AesseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
